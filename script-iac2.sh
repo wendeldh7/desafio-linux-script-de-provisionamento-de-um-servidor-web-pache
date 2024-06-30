64b21dd94fad84a141927d47967da4f290f20c29
Wendel Gomes <85760216+wendeldh7@users.noreply.github.com>
2024-06-30 13:30:52 -0400

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -3,17 +3,25 @@
 echo "Atualizando o servidor..."
 apt-get update
 apt-get upgrade -y
+
+echo "Instalando o apache2..."
 apt-get install apache2 -y
+
+echo "Instalando o unzip..."
 apt-get install unzip -y
 
+echo "Instalando o git..."
+apt installl git -y
 
-echo "Baixando e copiando os arquivos da aplicação..."
-
+echo "Baixando a aplicação..."
 cd /tmp
-wget https://github.com/wendeldh7/linux-site-dio/archive/refs/heads/main.zip
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactar a aplicação..."
 unzip main.zip
+
+echo "Copiar a aplicação para o servidor..."
 cd linux-site-dio-main
 cp -R * /var/www/html/
 
-
-
+echo "Finalizado"
